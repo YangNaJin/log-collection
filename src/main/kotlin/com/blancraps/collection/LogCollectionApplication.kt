31c13c055fcc7abc55943715c3c6a7dcb15f6bc5
jy.yang <jy.yang@tg360tech.com>
2021-09-14 17:43:28 +0900

    initial commit

---
+++ src/main/kotlin/com/blancraps/collection/LogCollectionApplication.kt
@@ -0,0 +1,11 @@
+package com.blancraps.collection
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LogCollectionApplication
+
+fun main(args: Array<String>) {
+    runApplication<LogCollectionApplication>(*args)
+}
